3d188d36cb62b1f7af30ea734cf3d26b74d55218
Viacheslav Romanov <kozyr52@gmail.com>
2021-11-12 01:13:55 +0300

    first commit

--- homework_3.js
+++ homework_3.js
@@ -291,7 +291,25 @@
 //     i++;
 // }
 
-for (let i = 0; i <= 100; i++) {
-    if (i % 2 == 0)
-    console.log(i);
-}
+// for (let i = 0; i <= 100; i++) {
+//     if (i % 2 == 0)
+//     console.log(i);
+// }
+
+// 4. С помощью цикла найдите сумму чисел от 1 до 100.
+
+// let sum = 0
+// for (let i = 1; i <= 100; i++) {
+//     sum = sum + i
+// }
+// console.log(sum);
+
+// let sum = 0
+// let i = 0;
+
+// while (i <= 100) {
+//     sum = sum + i
+//     i++;
+// }
+
+// console.log(sum);
